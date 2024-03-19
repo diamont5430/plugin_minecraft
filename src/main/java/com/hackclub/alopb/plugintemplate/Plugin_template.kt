d3bf8bbd9eaea090463c413de1257547aee30092
diamont5430 <145464000+diamont5430@users.noreply.github.com>
2024-03-19 15:22:59 +0100

    Initial commit

---
+++ src/main/java/com/hackclub/alopb/plugintemplate/Plugin_template.kt
@@ -0,0 +1,13 @@
+package com.hackclub.alopb.plugintemplate
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Plugin_template : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
